7d29e88305bec73c48107975b4aa5c499f8e9bbc
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-03 13:41:28 -0400

    Add games

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,29 @@ export default function App(){
 
     return(
         <>
-
+        <h1>Biblioteca de jogos</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title">Título:</label>
+            <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor="cover">Capa:</label>
+            <input type="text" id="cover" value={cover} onChange={(e) => setCover(e.target.value)} />
+          </div>
+          <button type="submit">Adicionar a bibilioteca</button>
+        </form>
+        <div className="games">
+          {games.map((game) => (
+            <div key={game.id}>
+              <img src={game.cover} alt={game.title} />
+              <div>
+                <h2>{game.title}</h2>
+                <button>Remover</button>
+              </div>
+            </div>
+          ))}
+        </div>
         </>
     )
 }
